f3da3ec4c4faf8106bb357fb2a376a0a619fed41
OPTEAMIX\AAKKarapaka <aakkarapaka@opteamix.com>
2025-07-07 13:54:30 +0530

    Single Response Design changes

---
+++ elasticsearch-backend/src/main/java/com/example/demo/configs/CustomConverters.java
@@ -0,0 +1,40 @@
+package com.example.demo.configs;
+
+import com.example.demo.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchCustomConversions;
+import org.springframework.lang.NonNull;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+
+@Configuration
+public class Converters {
+
+    @Bean
+    public ElasticsearchCustomConversions elasticsearchCustomConversions() {
+        return new ElasticsearchCustomConversions(Arrays.asList(new LocalDateTimeToLongConverter(), new LongToLocalDateTimeConverter()));
+    }
+
+    @ReadingConverter
+    public static class LongToLocalDateTimeConverter implements Converter<Long, LocalDateTime> {
+        @Override
+        public LocalDateTime convert(@NonNull Long source) {
+            return Instant.ofEpochMilli(source).atZone(ZoneId.systemDefault()).toLocalDateTime();
+        }
+    }
+
+    @WritingConverter
+    public static class LocalDateTimeToLongConverter implements Converter<LocalDateTime, Long> {
+        @Override
+        public Long convert(@NonNull LocalDateTime source) {
+            return source.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+        }
+    }
+}
